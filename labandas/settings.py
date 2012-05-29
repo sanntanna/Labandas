5a1f58ddf85938fab25d6137548dc62c6a0e819c
Renato Menegasso <rhmenegasso@gmail.com>
2012-05-28 23:18:44 -0300

    Adicionando pacote de anuncios

--- labandas/settings.py
+++ labandas/settings.py
@@ -122,12 +122,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'equipaments',
     'medias',
     'bands',
     'solicitations',
+    'announcements',
 )
 
 # A sample logging configuration. The only tangible logging
