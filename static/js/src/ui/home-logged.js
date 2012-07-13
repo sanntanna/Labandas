8eafa5e8ce8fa0b00740cc34a857c6fad68c514f
Renato Menegasso <rhmenegasso@gmail.com>
2012-07-13 00:12:26 -0300

    Implementando front-end na home logada

---
+++ static/js/src/ui/home-logged.js
@@ -0,0 +1,37 @@
+(function(){
+	this.init = function(){};
+	
+	this.domLoaded = function(){
+		setupInlineEdition();
+		setupSolicitations();
+	};
+	
+	function setupInlineEdition(){
+		$("form.inline-form input").change(function(){
+			$(this).closest('form').trigger('submit');
+		});
+	}
+	
+	function setupSolicitations(){
+		$(".accept-solicitation, .decline-solicitation").click(function(e){
+			e.preventDefault();
+			
+			var link = $(this),
+				isAccepting = link.hasClass('accept-solicitation'),
+				url = "/solicitacao/" + (isAccepting ? 'aceitar' : 'recusar');
+			
+			$.post(url, {id: link.attr('data-id')}, function(response){
+				link.closest('.solicitation').hide(700, function(){
+					$(this).remove();
+				});
+				
+				if(isAccepting){
+					new lb.message("Agora você está na banda #banda#", lb.message.SUCCESS);
+				}
+			});
+		});
+	}
+	
+	this.init();
+	$(this.domLoaded);
+})();
--- static/js/src/ui/home.js
+++ static/js/src/ui/home.js
@@ -3,8 +3,6 @@
 	
 	this.domLoaded = function(){
 		setupSubscribeForm();
-		setupInlineEdition();
-		setupSolicitations();
 	};
 	
 	function setupSubscribeForm(){
@@ -13,32 +11,6 @@
 				location.reload();
 				return;
 			}
-		});
-	}
-	
-	function setupInlineEdition(){
-		$("form.inline-form input").change(function(){
-			$(this).closest('form').trigger('submit');
-		});
-	}
-	
-	function setupSolicitations(){
-		$(".accept-solicitation, .decline-solicitation").click(function(e){
-			e.preventDefault();
-			
-			var link = $(this),
-				isAccepting = link.hasClass('accept-solicitation'),
-				url = "/solicitacao/" + (isAccepting ? 'aceitar' : 'recusar');
-			
-			$.post(url, {id: link.attr('data-id')}, function(response){
-				link.closest('.solicitation').hide(700, function(){
-					$(this).remove();
-				});
-				
-				if(isAccepting){
-					new lb.message("Agora você está na banda #banda#", lb.message.SUCCESS);
-				}
-			});
 		});
 	}
 	
