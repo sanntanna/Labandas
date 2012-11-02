b12a30cbee2623068bc85707b56c32b5a99b006a
Renato Menegasso <rhmenegasso@gmail.com>
2012-11-02 00:18:04 -0200

    Ajustando problemas de encoding na LB + adicionando arquivo css

--- precadastro/forms.py
+++ precadastro/forms.py
@@ -3,5 +3,12 @@ from django import forms
 from models import Record
 
 class RecordForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(RecordForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = "Nome"
+        self.fields['instrument'].label = "Instrumentos que toca"
+        self.fields['uf'].label = "Estado"
+
     class Meta:
         model = Record
