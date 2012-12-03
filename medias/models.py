ab3c97e179876d9371993e4c846af746eef69cff
Renato Menegasso <rhmenegasso@gmail.com>
2012-12-03 20:10:54 -0200

    Implementando alteracao de player do sound cloud

--- medias/models.py
+++ medias/models.py
@@ -1,7 +1,8 @@
 #coding=ISO-8859-1
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 from django.db import models
 from utils import ImageHandler, AmazonS3
-
 
 class MediaType(models.Model):
     name = models.CharField(max_length=50)
@@ -9,7 +10,7 @@ class MediaType(models.Model):
         return self.name
 
 class Media(models.Model):
-    media = models.CharField(max_length=50)
+    media = models.CharField(max_length=255)
     media_type = models.ForeignKey(MediaType)
     def __unicode__(self):
         return "%s %d" % (self.media_type.name, self.id)
@@ -20,41 +21,25 @@ class MusicianMedia(models.Model):
     AVATAR_SMALL_IMG_NAME = "avatar-small.png"
     COVER_IMG_NAME = "cover.png"
 
-    __avatar_type = None
-    __cover_photo_type = None
-
-    @property
-    def AVATAR_TYPE(self):
-        if self.__avatar_type is None:
-            self.__avatar_type = MediaType.objects.get(name="avatar")
-        return self.__avatar_type
-
-    @property
-    def COVER_PHOTO_TYPE(self):
-        if self.__cover_photo_type is None:
-            self.__cover_photo_type = MediaType.objects.get(name="cover_photo")
-        return self.__cover_photo_type
-
     media_list = models.ManyToManyField(Media, null=True, blank=True)
 
-    def avatar():
-        def fget(self):
-            if not self.media_list.filter(media_type = self.AVATAR_TYPE):
-                return None
-            return "%s%d/%s" % (self.BASE_URL_USER_IMAGES, self.musician.id, self.AVATAR_IMG_NAME)
+    def get_avatar(self):
+        if not self.media_list.filter(media_type__name = "avatar"):
+            return None
+        return "%s%d/%s" % (self.BASE_URL_USER_IMAGES, self.musician.id, self.AVATAR_IMG_NAME)
 
-        def fset(self, uploaded_image):
-            handler = ImageHandler()
-            images = handler.handle_profile_images(uploaded_image)
-            
-            s3 = AmazonS3()
-            s3.upload_file(images['default'], '/u/%d/%s' % (self.musician.id, self.AVATAR_IMG_NAME))
-            s3.upload_file(images['small'], '/u/%d/%s' % (self.musician.id, self.AVATAR_SMALL_IMG_NAME))
+    def set_avatar(self, uploaded_image):
+        handler = ImageHandler()
+        images = handler.handle_profile_images(uploaded_image)
+        
+        s3 = AmazonS3()
+        s3.upload_file(images['default'], '/u/%d/%s' % (self.musician.id, self.AVATAR_IMG_NAME))
+        s3.upload_file(images['small'], '/u/%d/%s' % (self.musician.id, self.AVATAR_SMALL_IMG_NAME))
 
-            if not self.media_list.filter(media_type = self.AVATAR_TYPE):
-                self.media_list.add(Media.objects.create(media = "avatar", media_type = self.AVATAR_TYPE))
-        return locals()
-    avatar = property(**avatar())
+        if not self.media_list.filter(media_type__name = "avatar"):
+            self.media_list.add(Media.objects.create(media = "avatar", media_type = self.__type("avatar")))
+
+    avatar = property(get_avatar, set_avatar)
 
     @property
     def avatar_small(self):
@@ -63,24 +48,51 @@ class MusicianMedia(models.Model):
         return "%s%d/%s" % (self.BASE_URL_USER_IMAGES, self.musician.id, self.AVATAR_SMALL_IMG_NAME)
 
 
-    def cover():
-        def fget(self):
-            if not self.media_list.filter(media_type = self.COVER_PHOTO_TYPE):
-                return None
-            return "%s%d/%s" % (self.BASE_URL_USER_IMAGES, self.musician.id, self.COVER_IMG_NAME)
+    def get_cover(self):
+        if not self.media_list.filter(media_type__name = "cover_photo"):
+            return None
+        return "%s%d/%s" % (self.BASE_URL_USER_IMAGES, self.musician.id, self.COVER_IMG_NAME)
 
-        def fset(self, uploaded_image):
-            handler = ImageHandler()
-            images = handler.handle_cover_image(uploaded_image)
-            
-            s3 = AmazonS3()
-            s3.upload_file(images['default'], '/u/%d/%s' % (self.musician.id, self.COVER_IMG_NAME))
+    def set_cover(self, uploaded_image):
+        handler = ImageHandler()
+        images = handler.handle_cover_image(uploaded_image)
+        
+        s3 = AmazonS3()
+        s3.upload_file(images['default'], '/u/%d/%s' % (self.musician.id, self.COVER_IMG_NAME))
 
-            if not self.media_list.filter(media_type = self.COVER_PHOTO_TYPE):
-                self.media_list.add(Media.objects.create(media = "cover", media_type = self.COVER_PHOTO_TYPE))
-        return locals()
-    cover = property(**cover())
+        if not self.media_list.filter(media_type__name = "cover_photo"):
+            self.media_list.add(Media.objects.create(media = "cover", media_type = self.__type("cover_photo")))
 
+    cover = property(get_cover, set_cover)
+
+
+    def get_sound_cloud(self):
+        objs = self.media_list.filter(media_type__name = "sound_cloud")
+        return None if len(objs) == 0 else objs[0]
+    
+    def set_sound_cloud(self, value):
+        validate = URLValidator(verify_exists=False)
+
+        try:
+            validate(value)
+            if value.find("soundcloud.com/") == -1:
+                raise ValidationError()
+        except ValidationError, e:
+            raise ValidationError("Url do sound cloud invalida")
+
+        sound_cloud = self.get_sound_cloud()
+
+        if sound_cloud is None:
+            self.media_list.add(Media.objects.create(media = value, media_type = self.__type("sound_cloud")))
+            return
+
+        sound_cloud.media = value
+        sound_cloud.save()
+
+    sound_cloud = property(get_sound_cloud, set_sound_cloud)
+
+    def __type(self, name):
+        return MediaType.objects.get(name=name)
 
     def __unicode__(self):
         return "Midia %s" % self.id
