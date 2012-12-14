dc3887ef344f82bde59a259fcd33f23249f5609c
Anderson Santana <andersonsantana@MacBook-Air.local>
2012-12-14 17:14:41 -0200

    Ajustando xpath para recuperar o no de erro

--- geoapi/models.py
+++ geoapi/models.py
@@ -62,7 +62,7 @@ class AddressFinder(object):
     def get_address(self, cep):
         page = pq(url=self.CORREIOS_URL, method="POST", data={'relaxation': cep, 'Metodo':'listaLogradouro', 'TipoConsulta': 'relaxation'})
         content = page.find(".ctrlcontent > div table:first tr")
-        error = page.find("title:contains('Erro')").html()
+        error = page.find("title")[1].text
         
         result = SearchAddrResult()
         
