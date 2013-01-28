ebb68381f1226465cfe269226521345c17767688
Renato Menegasso <rhmenegasso@gmail.com>
2013-01-28 21:55:02 -0200

    Ajustando urls quebradas

--- bands/band_urls.py
+++ bands/band_urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns
 
 urlpatterns = patterns('bands.band_views',
-    (r'^cadastrar$', 'add_band'),
-    (r'^cadastrar/post$', 'add_band_post'),
+    (r'^criar$', 'create_band'),
     (r'^editar/(?P<band_id>\d+)$', 'edit_band'),
     (r'^editar/(?P<band_id>\d+)/post$', 'edit_band_post'),
     (r'^(?P<name>[a-z0-9-]+)/(?P<band_id>\d+)$', 'band_page'),
