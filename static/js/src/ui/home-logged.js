9834b9b4e673b9cf8187b212a2d67a2d597ab283
Anderson Santana <andersonsantana@MacBook-Air.local>
2013-04-03 00:25:43 -0300

    inicio de implementação do Slider de páginas

--- static/js/src/ui/home-logged.js
+++ static/js/src/ui/home-logged.js
@@ -9,6 +9,7 @@
 		setupSoundCloud();
 		musicalStyles();
 		bandCreation();
+		slider();
 	};
 	
 	
@@ -180,6 +181,30 @@
 			if(!response.success){ return; }
 			location.href = response.band_page_url;
 		});
+	}
+
+	function slider(){
+		$(window).bind('popstate', function (e) {
+	        if (e.originalEvent.state && e.originalEvent.state.path) {
+	            $.get(e.originalEvent.state.path, function(data) {
+	                $('#container').slideTo(data);      
+	            });
+	            return false;
+	        }
+        	return true;
+    	}
+
+    	$.fn.slideTo = function(data) {
+        	var width = parseInt($('#container').css('width'));
+        	var transfer = $('<div class="transfer"></div>').css({ 'width': (2 * width) + 'px' });
+        	var current = $('<div class="current"></div>').css({ 'width': width + 'px', 'left': '0', 'float': 'left' }).html($('#container').html());
+        	var next = $('<div class="next"></div>').css({ 'width': width + 'px', 'left': width + 'px', 'float': 'left' }).html(data);
+        	transfer.append(current).append(next);
+	        $('#container').html('').append(transfer);
+	        transfer.animate({ 'margin-left': '-' + width + 'px' }, 300, function () {
+	            $('#container').html(data);
+	        });
+    	}
 	}
 
 	this.init();
